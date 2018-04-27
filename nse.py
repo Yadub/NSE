1ae7f2ec4491854acc13a2d3c8b9d1d784674f70
Yadu Raj Bhageria <yaduraj1993@gmail.com>
2018-04-27 13:30:24 +0530

    NSE file added downloading index functionality

--- nse.py
+++ nse.py
@@ -1,8 +1,20 @@
 import requests
 import pandas as pd
 import os
+
 # ---------------------------------------------------------------------------- #
-def download_NSE(df=None,offset_date=pd.Timestamp('today'),save_path=None):
+def url_NSEIndex(offset_date=pd.Timestamp('today')):
+    url_start = 'https://www.nseindia.com/content/indices/ind_close_all_'
+    year = offset_date.strftime('%Y')
+    month = offset_date.strftime('%m')
+    day = offset_date.strftime('%d')
+    url_end = day + month + year + '.csv'
+    url = url_start + url_end
+
+    return url
+
+# ---------------------------------------------------------------------------- #
+def url_NSE(offset_date=pd.Timestamp('today')):
 
     year = offset_date.strftime('%Y')
     month = offset_date.strftime('%b').upper()
@@ -13,45 +25,95 @@ def download_NSE(df=None,offset_date=pd.Timestamp('today'),save_path=None):
     url_end = 'cm' + day + month + year + 'bhav.csv.zip'
     url =  url_start + year + url_join + month + url_join + url_end
 
+    return url
+
+# ---------------------------------------------------------------------------- #
+def download_NSEdata(df=None,url=url_NSE(pd.Timestamp('today')),save_path=None,date=None):
+
+    if date is None:
+        date = '?'
+    else:
+        date = date.date()
+
+    if url[-3:] == 'zip':
+        tempFileName = 'downloadedFile.zip'
+    else:
+        tempFileName = 'downloadedFile'
+
     try:
         # download the file contents in binary format
         r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 
         # open method to open a file on your system and write the contents
-        with open("zippedFile.zip", "wb") as code:
+        with open(tempFileName, "wb") as code:
             code.write(r.content)
 
-        # read the zip file as a pandas dataframe
-        df_new = pd.read_csv('zippedFile.zip')   # pandas version 0.18.1 takes zip files
+        # read and return the zip file as a pandas dataframe
+        df_new = pd.read_csv(tempFileName)
 
-        # remove downloaded zipfile
-        os.remove('zippedFile.zip')
+        # Remove the downloaded temp file
+        os.remove(tempFileName)
 
         if len(df_new) == 1:
-            raise ValueError('No data avialable for this day!')
+            raise ValueError('No data available for this day!')
 
         if df is not None:
             df = df.append(df_new, ignore_index=True)
         else:
             df = df_new
 
+        # save the new data frame
         if save_path is not None:
-            # save the new data frame
             df.to_csv(save_path, encoding='utf-8', index=False)
 
-        print("Saved NSE data for: %s" %((day+month+year)))
+        print("Saved data for: %s" %(date))
 
     except:
-        print("No NSE data for ::(failed):: %s" %((day+month+year)))
+        print("No data for :::::::::::: %s" %(date))
 
     return df
 
+# ---------------------------------------------------------------------------- #
+def download_NSEIndex_as_csv(df=None,save_path='NSEIndex_Data.csv',num_days_back=365):
+
+    for d in range(num_days_back):
+        offset_date = (pd.Timestamp('today') - pd.DateOffset(days=d))
+        df = download_NSEdata(df, url_NSEIndex(offset_date), save_path, offset_date)
+
+    return df
+
+# ---------------------------------------------------------------------------- #
+def update_NSEIndex(NSE_data_path, save_path=None, max_days_back=30):
+
+    if save_path == None:
+        save_path=NSE_data_path
+
+    downloaded = False
+
+    df = pd.read_csv(NSE_data_path)
+    # Get the last date avaiable in the previous data
+    last_date = max(pd.to_datetime(df['Index Date'], format='%d-%m-%Y'))
+
+    for d in range(max_days_back):
+        offset_date = (pd.Timestamp('today') - pd.DateOffset(days=(d)))
+
+        if offset_date.date() == last_date.date():
+            print('Last date of data reached.')
+            break
+
+        df = download_NSEdata(df, url_NSEIndex(offset_date), date=offset_date)
+        downloaded = True
+
+    # Save data once and if everything is downloaded
+    if downloaded: df.to_csv(save_path, encoding='utf-8', index=False)
+
+    return df
 # ---------------------------------------------------------------------------- #
 def download_NSE_as_csv(df=None,save_path='NSE_Data.csv',num_days_back=365):
 
     for d in range(num_days_back):
         offset_date = (pd.Timestamp('today') - pd.DateOffset(days=d))
-        df = download_NSE(df,offset_date,save_path)
+        df = download_NSEdata(df, url_NSE(offset_date), save_path, offset_date)
 
     return df
 # ---------------------------------------------------------------------------- #
@@ -73,7 +135,7 @@ def update_NSE(NSE_data_path, save_path=None, max_days_back=30):
             print('Last date of data reached.')
             break
 
-        df = download_NSE(df,offset_date)
+        df = download_NSEdata(df, url_NSE(offset_date), date=offset_date)
         downloaded = True
 
     # Save data once and if everything is downloaded
@@ -82,8 +144,10 @@ def update_NSE(NSE_data_path, save_path=None, max_days_back=30):
     return df
 
 # ---------------------------------------------------------------------------- #
-# url = 'https://www.nseindia.com/products/content/sec_bhavdata_full.csv'
-# url = 'https://www.nseindia.com/content/historical/EQUITIES/2018/Apr/cm20Apr2018bhav.csv.zip'
 
-# download_NSE_as_csv(save_path='NSE_DataTest.csv',num_days_back=7)
-update_NSE('NSE_Data.csv',max_days_back=10)
+# download_NSE_as_csv(save_path='data_NSE.csv',num_days_back=7)
+# update_NSE('data_NSE.csv',max_days_back=10)
+
+save_path = 'data_NSEIndex.csv'
+download_NSEIndex_as_csv(save_path=save_path)
+update_NSEIndex(save_path)
